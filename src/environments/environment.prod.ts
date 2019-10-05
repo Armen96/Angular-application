4d042efd91daef084ad3ff2a50bdb5d89c5360d9
Armen96 <barsegyan96armen@gmail.com>
2019-10-05 18:00:27 +0400

    Firebase config fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDW490BtQkcFf9q1QpDLeWwmIrIVWwS32k",
+    authDomain: "angular-app-fe489.firebaseapp.com",
+    databaseURL: "https://angular-app-fe489.firebaseio.com",
+    projectId: "angular-app-fe489",
+    storageBucket: "",
+    messagingSenderId: "410269889902",
+    appId: "1:410269889902:web:ed3987a0d346b170c83d7f",
+    measurementId: "G-C8W7MC70SK"
+  }
 };
